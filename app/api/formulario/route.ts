9ea3368438b0e08ff6b73f436062b09793a41a04
roberto <xdstuart@gmail.com>
2025-03-03 12:49:23 -0300

    LRUCache

--- app/api/formulario/route.ts
+++ app/api/formulario/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
 import { Pool } from 'pg';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const LRU = require('lru-cache');
+const LRUCache = require('lru-cache');
+
+
 
 // Configuração do banco de dados
 const pool = new Pool({
@@ -10,10 +12,11 @@ const pool = new Pool({
 });
 
 // Configuração do rate limit (máximo de 5 tentativas a cada 15 minutos por IP)
-const rateLimit = new LRU({
+const rateLimit = new LRUCache({
   max: 100, // Máximo de IPs armazenados
   ttl: 15 * 60 * 1000, // 15 minutos (tempo de expiração dos registros)
 });
+
 
 // Função para validar CPF
 const validarCPF = (cpf: string) => {
